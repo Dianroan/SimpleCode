c331c1f4dcd2a7c826ebfa545c55461bb79a23b5
dianroan <diegorodriguez.mt@gmail.com>
2025-11-07 00:27:57 -0600

    port

--- backend/src/config/env.js
+++ backend/src/config/env.js
@@ -9,7 +9,7 @@ export const env = {
     host: process.env.DB_HOST || "localhost",
     port: Number(process.env.DB_PORT || 3306),
     user: process.env.DB_USER || "root",
-    password: process.env.DB_PASS || "123456",
+    password: process.env.DB_PASS || "",
     database: process.env.DB_NAME || "simplecode_db",
   },
 };
